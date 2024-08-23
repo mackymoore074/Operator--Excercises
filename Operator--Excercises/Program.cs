fac02c3f94ab1279e4750c617649f4b9489b6be3
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-08-22 20:04:35 -0700

    Alll Done

--- Operator--Excercises/Program.cs
+++ Operator--Excercises/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Operational_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            // Exercise 2
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with radius of {radius} is {AreaOfCircle(radius)}");
+        }
+
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
